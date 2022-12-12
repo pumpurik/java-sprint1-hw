769f7abe1aab0852e014f2b54656a18e41f8014c
Taisiya Shavikina <AikoUmio@yandex.ru>
2022-12-13 01:50:57 +0300

    Первая сдача

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,81 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Пришло время практики!");
+        StepTracker data = new StepTracker();
+        StepTracker.Converter convert = data.new Converter();
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            printMenu();
+            int command = scanner.nextInt();
+            if (command==1){
+                System.out.println("Введите номер месяца: ");
+                int month= scanner.nextInt();
+                while ((month<=0)||(month>12))
+                {
+                    System.out.println("Введите номер месяца: ");
+                    month= scanner.nextInt();
+                }
+                System.out.println("Введите номер дня: ");
+                int number_day= scanner.nextInt();
+                while ((number_day<=0)||(number_day>30))
+                {
+                    System.out.println("Введите номер дня: ");
+                    number_day= scanner.nextInt();
+                }
+                System.out.println("Введите кол-во шагов: ");
+                int steps= scanner.nextInt();
+                while (steps<0)
+                {
+                    System.out.println("Введите кол-во шагов: ");
+                    steps= scanner.nextInt();
+                }
+                data.enterSteps(month,number_day,steps);
+            }
+            else if (command ==2 ){
+                System.out.println("Введите номер месяца: ");
+                int month = scanner.nextInt();
+                while ((month<=0)||(month>12))
+                {
+                    System.out.println("Введите номер месяца: ");
+                    month= scanner.nextInt();
+                }
+                data.printStepsPerDay(month);
+                System.out.println("Общее количество шагов - " + data.countStepsPerMonth(month));
+                System.out.println("Максимальное пройденное количество шагов - " + data.countMaxSteps(month));
+                System.out.println("Среднее количество шагов в день составило - " + data.countAverageSteps(month));
+                System.out.println("Пройденная дистанция - " + convert.countDistance(month) + " км!");
+                System.out.println("Вы сожгли - " + convert.countCalories(month) + " килокалорий!");
+                System.out.println("Лучшая серия - " + data.bestSeriesDays(month) + " подряд идущих дней!");
+
+            }
+            else if (command == 3){
+                System.out.println("Введите новое целевое значение: ");
+                int newGoal = scanner.nextInt();
+                while (newGoal<=0)
+                {
+                    System.out.println("Введите новое целевое значение: ");
+                    newGoal = scanner.nextInt();
+                }
+                data.changeGoal(newGoal);
+            }
+            else if (command==0){
+                System.out.println("Выход из приложения");
+                break;
+            }
+            else{
+                System.out.println("Такой команды нет!");
+            }
+        }
+    }
+    public static void printMenu(){
+        System.out.println("");
+        System.out.println("Что вы хотите сделать?");
+        System.out.println("1 - ввести количество шагов за определённый день");
+        System.out.println("2 - напечатать статистику за определённый месяц");
+        System.out.println("3 - изменить цель по количеству шагов в день");
+        System.out.println("0 - выйти из приложения");
     }
 }
+
+
---
+++ src/StepTracker.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+public class StepTracker {
+    int goal = 10000;
+    MonthData[] monthToData;
+    public StepTracker() {
+        monthToData = new MonthData[12];
+        for (int i = 0; i < monthToData.length; i++) {
+            monthToData[i] = new MonthData();
+        }
+    }
+    class MonthData {
+        int[] day;
+        public MonthData() {
+            day=new int[30];
+            for (int i = 0; i < day.length; i++) {
+                day[i] = 0;
+            }
+        }
+    }
+
+    void enterSteps (int month, int num_day, int steps) {
+        monthToData[month - 1].day[num_day - 1] = steps;
+        System.out.println("Вы прошли: " + monthToData[month - 1].day[num_day - 1] + " шагов " + num_day + " числа " + month + " месяца!");
+    }
+    void printStepsPerDay (int month){
+        for (int i=0; i< monthToData[month-1].day.length; i++ ){
+            if (i<(monthToData[month-1].day.length-1)){
+                System.out.print((i+1) + " день: " + monthToData[month-1].day[i] + ", ");
+            }
+            else{
+                System.out.println((i+1) + " день: " + monthToData[month-1].day[i]);
+            }
+        }
+    }
+    int countStepsPerMonth (int month){
+        int sum=0;
+        for (int i=0; i< monthToData[month-1].day.length; i++){
+            sum=sum + monthToData[month-1].day[i];
+        }
+        return sum;
+    }
+    int countMaxSteps (int month){
+        int maxSteps=0;
+        for (int i=0; i< monthToData[month-1].day.length; i++){
+            if (monthToData[month-1].day[i] > maxSteps){
+                maxSteps=monthToData[month-1].day[i];
+            }
+        }
+        return maxSteps;
+    }
+    int countAverageSteps (int month){
+        int sum = countStepsPerMonth (month);
+        int average=sum/30;
+        return average;
+    }
+    void changeGoal(int newGoal){
+        goal=newGoal;
+        System.out.println("Новая цель по количеству шагов в день равна " + goal + "!");
+    }
+    int bestSeriesDays (int month) {
+        int countDays = 0;
+        int maxCountDays =0;
+        for (int i = 0; i < monthToData[month - 1].day.length; i++) {
+            if (monthToData[month - 1].day[i] >= goal) {
+                countDays=countDays+1;
+            }
+            else {
+                if(countDays>maxCountDays){
+                    maxCountDays = countDays;
+                }
+                countDays=0;
+            }
+        }
+        return maxCountDays;
+    }
+    public class Converter {
+        int centimeters = 75;
+        int calories = 50;
+        int countDistance (int month){
+            int sum = countStepsPerMonth (month);
+            int km=(sum*centimeters)/100000;
+            return km;
+        }
+        int countCalories (int month){
+            int sum = countStepsPerMonth (month);
+            int kilocalories=(sum*calories)/1000;
+            return kilocalories;
+        }
+    }
+}
